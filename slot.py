d9f786dce5b3a6d47fcf3f275ddedcc1748624d3
Tenshin22 <posohinvit09@gmail.com>
2026-02-13 19:39:49 +0700

    добавили проверку на наличие файлов

--- slot.py
+++ slot.py
@@ -1,14 +1,27 @@
 # импорты
 import random
+import os
+
+
+file_path_1 = "user_names.txt"
+file_path_2 = "users_coins.txt"
+
+if not os.path.exists(file_path_1) or not os.path.exists(file_path_2):
+    open(file_path_1, mode="w", encoding="utf-8").close()
+    open(file_path_2, mode="w", encoding="utf-8").close()
+
 
 DEBUG = False
 
 # списки
-symbols = [ "💎","🍒","🏆",]
+symbols = [
+    "💎",
+    "🍒",
+    "🏆",
+]
 list_droppeds_symbols_1 = []
 list_droppeds_symbols_2 = []
 list_droppeds_symbols_3 = []
-
 
 
 # выподение символов
@@ -17,20 +30,21 @@ def symbols_cosino(symbols, droppeds_symbols):
     while number <= 3:
         random_sheble = random.choice(symbols)
         droppeds_symbols.append(random_sheble)
-        
+
         print(random_sheble, end=" ")
-        
-        
+
         number += 1
     print()
-    
-    
+
     return droppeds_symbols
 
 
 # проверка на выйгрыш по гаризонтали
 def cheak_on_win(coins, droppeds_symbols):
-    if droppeds_symbols[0] == droppeds_symbols[1] and droppeds_symbols[0] == droppeds_symbols[2]:
+    if (
+        droppeds_symbols[0] == droppeds_symbols[1]
+        and droppeds_symbols[0] == droppeds_symbols[2]
+    ):
         print("+20💸")
         coins += 20
     elif droppeds_symbols[0] == droppeds_symbols[1]:
@@ -41,71 +55,89 @@ def cheak_on_win(coins, droppeds_symbols):
         coins += 5
     else:
         print("+0💸")
-    
-    
+
     return coins
 
 
 # проверка на выйгрыш по диагонали
-def cheak_diagonally(coins, droppeds_symbols_1,
-                     droppeds_symbols_2, droppeds_symbols_3):
-    if droppeds_symbols_1[2] == droppeds_symbols_2[1] and droppeds_symbols_1[2] == droppeds_symbols_3[0]:
+def cheak_diagonally(coins, droppeds_symbols_1, droppeds_symbols_2, droppeds_symbols_3):
+    if (
+        droppeds_symbols_1[2] == droppeds_symbols_2[1]
+        and droppeds_symbols_1[2] == droppeds_symbols_3[0]
+    ):
         print("+30💸")
         coins += 30
-    elif  droppeds_symbols_1[0] == droppeds_symbols_2[1] and droppeds_symbols_1[0] == droppeds_symbols_3[2]:
+    elif (
+        droppeds_symbols_1[0] == droppeds_symbols_2[1]
+        and droppeds_symbols_1[0] == droppeds_symbols_3[2]
+    ):
         print("+30💸")
         coins += 30
     else:
         print("+0💸")
-    
-    
+
     return coins
 
 
 # проверка на выйгрыш по вертикали
-def cheak_vertically(coins, droppeds_symbols_1,
-                     droppeds_symbols_2, droppeds_symbols_3):
-    if droppeds_symbols_1[0] == droppeds_symbols_2[0] and droppeds_symbols_1[0] == droppeds_symbols_3[0]:
+def cheak_vertically(coins, droppeds_symbols_1, droppeds_symbols_2, droppeds_symbols_3):
+    if (
+        droppeds_symbols_1[0] == droppeds_symbols_2[0]
+        and droppeds_symbols_1[0] == droppeds_symbols_3[0]
+    ):
         print("+25💸")
         coins += 25
-    elif droppeds_symbols_1[1] == droppeds_symbols_2[1] and droppeds_symbols_1[1] == droppeds_symbols_3[1]:
+    elif (
+        droppeds_symbols_1[1] == droppeds_symbols_2[1]
+        and droppeds_symbols_1[1] == droppeds_symbols_3[1]
+    ):
         print("+25💸")
         coins += 25
-    elif droppeds_symbols_1[2] == droppeds_symbols_2[2] and droppeds_symbols_1[2] == droppeds_symbols_3[2]:
+    elif (
+        droppeds_symbols_1[2] == droppeds_symbols_2[2]
+        and droppeds_symbols_1[2] == droppeds_symbols_3[2]
+    ):
         print("+25💸")
         coins += 25
     else:
         print("+0💸")
-    
-    
+
     return coins
 
 
 # игра
-def play(coins, symbols, list_droppeds_symbols_1, 
-         list_droppeds_symbols_2, list_droppeds_symbols_3,
-         user_name):
-    droppeds_symbols_1 = symbols_cosino(symbols, droppeds_symbols=list_droppeds_symbols_1)
-    droppeds_symbols_2 = symbols_cosino(symbols, droppeds_symbols=list_droppeds_symbols_2)
-    droppeds_symbols_3 = symbols_cosino(symbols, droppeds_symbols=list_droppeds_symbols_3)
-        
+def play(
+    coins,
+    symbols,
+    list_droppeds_symbols_1,
+    list_droppeds_symbols_2,
+    list_droppeds_symbols_3,
+    user_name,
+):
+    droppeds_symbols_1 = symbols_cosino(
+        symbols, droppeds_symbols=list_droppeds_symbols_1
+    )
+    droppeds_symbols_2 = symbols_cosino(
+        symbols, droppeds_symbols=list_droppeds_symbols_2
+    )
+    droppeds_symbols_3 = symbols_cosino(
+        symbols, droppeds_symbols=list_droppeds_symbols_3
+    )
 
     coins = cheak_on_win(coins, droppeds_symbols=droppeds_symbols_1)
     coins = cheak_on_win(coins, droppeds_symbols=droppeds_symbols_2)
     coins = cheak_on_win(coins, droppeds_symbols=droppeds_symbols_3)
 
+    coins = cheak_diagonally(
+        coins, droppeds_symbols_1, droppeds_symbols_2, droppeds_symbols_3
+    )
 
-    coins = cheak_diagonally(coins, droppeds_symbols_1,
-                             droppeds_symbols_2, droppeds_symbols_3)
-
-
-    coins = cheak_vertically(coins, droppeds_symbols_1,
-                             droppeds_symbols_2, droppeds_symbols_3)
-
+    coins = cheak_vertically(
+        coins, droppeds_symbols_1, droppeds_symbols_2, droppeds_symbols_3
+    )
 
     print(f"{user_name} у вас {coins}")
-    
-    
+
     return coins
 
 
@@ -120,10 +152,8 @@ def recording_names(user_name, list_users_names):
 
 
 # запись выйгрыша пользывателя
-def recording_coins(coins, list_users_coins,
-                    list_users_names, user_name):
-    
-        
+def recording_coins(coins, list_users_coins, list_users_names, user_name):
+
     if user_name not in list_users_names:
         with open("users_coins.txt", mode="a", encoding="utf-8") as f:
             coins = str(coins) + "\n"
@@ -131,34 +161,27 @@ def recording_coins(coins, list_users_coins,
     else:
         coins_play = coins
         i = list_users_names.index(user_name)
-        
-        
+
         if DEBUG:
             print("индекс где находится имя:", i)
             print("list_users_coins", list_users_coins)
             print("list_users_coins", list_users_names)
-        
-        
+
         previous_coins = int(list_users_coins[i])
-        
-        
+
         if coins_play > previous_coins:
             coins_play = str(coins_play)
-            
-            
+
             list_users_coins[i] = coins_play
             print(list_users_coins)
-            
-            
+
             index = 0
-            
-            
+
             with open("users_coins.txt", mode="w", encoding="utf-8") as f:
                 while index <= len(list_users_coins) - 1:
                     coins = list_users_coins[index] + "\n"
                     f.write(coins)
-                        
-                        
+
                     index += 1
         else:
 
@@ -169,37 +192,32 @@ def recording_coins(coins, list_users_coins,
 def get_users_names() -> list[str]:
     with open("user_names.txt", mode="r", encoding="utf-8") as f:
         temporary_list = f.readlines()
-        
-        
+
         list_users_names = []
         index = 0
         while index <= len(temporary_list) - 1:
             list_users_names.append(temporary_list[index].replace("\n", ""))
-            
-            
+
             index += 1
-        
-         
+
         return list_users_names
 
 
 # открываем выйгрыш пользывателей
-def get_users_coins() -> list[str]: 
+def get_users_coins() -> list[str]:
     with open("users_coins.txt", mode="r", encoding="utf-8") as f:
         temporary_list = f.readlines()
-            
-        print("temporary_list:", temporary_list) 
-        list_users_coins = []    
+
+        print("temporary_list:", temporary_list)
+        list_users_coins = []
         index = 0
         while index <= len(temporary_list) - 1:
             print("цикл работает")
             list_users_coins.append(temporary_list[index].replace("\n", ""))
-            
-            
+
             index += 1
-        
-         
-        return list_users_coins 
+
+        return list_users_coins
 
 
 # переменные
@@ -219,26 +237,31 @@ if user_name in list_users_names:
 else:
     print("Новичок не связывайся с казино")
 
-    
+
 # цикл игры
 while True:
-    user_choice = int(input("Каждая попытка стоит 15💸\n1.Продолжить\n2.Уйти\nНажмите, цифру: "))
+    user_choice = int(
+        input("Каждая попытка стоит 15💸\n1.Продолжить\n2.Уйти\nНажмите, цифру: ")
+    )
     if coins >= 15 and user_choice == 1:
         coins = coins - 15
-        coins = play(coins, symbols, list_droppeds_symbols_1,
-                     list_droppeds_symbols_2, list_droppeds_symbols_3,
-                     user_name)
+        coins = play(
+            coins,
+            symbols,
+            list_droppeds_symbols_1,
+            list_droppeds_symbols_2,
+            list_droppeds_symbols_3,
+            user_name,
+        )
     elif user_choice == 2:
         print(f"{user_name} вот ваш выйгрыш.\n В размере {coins}")
-        
+
         # читаем файл с рельтатами играков
         list_users_coins: list[str] = get_users_coins()
         print("list_users_coins:", list_users_coins)
-        
-        
+
         recording_names(user_name, list_users_names)
-        recording_coins(coins, list_users_coins,
-                        list_users_names, user_name)
+        recording_coins(coins, list_users_coins, list_users_names, user_name)
         exit()
     else:
         print(f"{user_name} у вас закончились деньги на игру")
